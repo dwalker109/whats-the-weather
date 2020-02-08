d4d423fba15607b2789cac8d0092ba8fe81e0118
Dan Walker <dan@dans-macbook.home>
2020-02-08 16:51:42 +0000

    Adds clickable history component
    Closes #6

---
+++ src/components/WeatherHistory/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const WeatherHistory = ({ weatherData, onClick }) => (
+  <div className="wh">
+    <ul className="wh__city">
+      {weatherData.map((city, key) => (
+        <li key={key} onClick={() => onClick(key)}>
+          {city.name}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default WeatherHistory;
--- src/components/WeatherPanel/index.jsx
+++ src/components/WeatherPanel/index.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import WeatherDetail from "../WeatherDetail";
+import WeatherHistory from "../WeatherHistory";
 import WeatherSearch from "../WeatherSearch";
 
 const WeatherPanel = () => {
-  const [weatherData, setWeatherData] = useState();
+  const [weatherData, unshiftWeatherData] = useReducer(
+    (history, latest) => [latest, ...history.slice(0, 5)],
+    []
+  );
+
+  const [activeCity, setActiveCity] = useState(0);
 
   return (
     <div className="wp">
@@ -12,14 +18,20 @@ const WeatherPanel = () => {
       </header>
       <main>
         <section className="wp__input">
-          <WeatherSearch handleSearchResult={setWeatherData} />
+          <WeatherSearch handleSearchResult={unshiftWeatherData} />
         </section>
         <section className="wp__detail">
-          {weatherData && <WeatherDetail weatherData={weatherData} />}
+          {weatherData.length > 0 && (
+            <WeatherDetail weatherData={weatherData[activeCity]} />
+          )}
         </section>
         <section className="wp__timer"></section>
       </main>
-      {/* <aside className="wp__history"></aside> */}
+      <aside className="wp__history">
+        {weatherData.length > 1 && (
+          <WeatherHistory weatherData={weatherData} onClick={setActiveCity} />
+        )}
+      </aside>
     </div>
   );
 };
